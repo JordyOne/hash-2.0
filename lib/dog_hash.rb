fbd949f1671a1a2ad83a5fa16163e624f15947f0
Jordan Dale <Jordan.T.Dale@gmail.com>
2014-07-09 10:40:17 -0600

    Dog hash warmup created and completed

---
+++ lib/dog_hash.rb
@@ -0,0 +1,14 @@
+DOG = {
+    :cattle_dog => {
+        :preferences => [{:name => "Harleigh", :favorite_toy => "frog"}],
+        :names => ["Harleigh", "brown"]
+    },
+    "harleigh" => {
+        :preferences => [{:favorite_toy => "frog"}],
+        :information => {:sport => ["frisbee", "hiking"]}
+    },
+    "trixie"   => {
+        :preferences => [{:favorite_food => "steak"}],
+        :information => {:sport => [:swimming, :prancing]}
+    }
+}
